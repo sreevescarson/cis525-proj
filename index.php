835ed35661d553e595f932e90313c841acbf2829
root <root@ip-172-31-36-83.us-east-2.compute.internal>
2019-11-07 03:51:04 +0000

    [IMP] docker-compose to create container environment

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<h1>Hello World!</h1>
+<h4>Attempting MySQL connection from php...</h4>
 <?php
-print("Hello World, PHP is rendering this text")	
-?>
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL successfully!";
+}
+
+?>
